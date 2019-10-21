e79a07e19b73ec0951f97d973dcdb0560a52eaa9
Ana Paola Treviño <anapao.trevino@gmail.com>
2019-10-20 20:59:53 -0500

    Lab6 - Weather App Heroku

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const climate = require('./climate.js')
+
+const port = process.env.PORT || 3000
+
+app.get('/weather', function(req, res) {
+    if (!req.query.search) {
+        res.send({
+            error: 'Debes enviar una ciudad'
+        })
+    } else {
+        const city = req.query.search
+        climate.getGeocode(city, function(error, coordinates) {
+            if (error) {
+                return res.send({
+                    error: error
+                })
+            } else {
+                if (coordinates) {
+                    climate.getWeather(coordinates, function(error, weather) {
+                        if (error) {
+                            return res.send({
+                                error: error
+                            })
+                        } else {
+                            const sentence = weather.summary
+                            + ' La temperatura actualmente esta a ' + weather.temperature + 'C '
+                            + 'y se siente a ' + weather.apparentTemperature + 'C. '
+                            + 'Hay ' + weather.rain + '% de probabilidad de lluvia.'
+
+                            res.send({
+                                city: city,
+                                summary: weather.summary,
+                                temperature: weather.temperature,
+                                apparentTemperature: weather.apparentTemperature,
+                                rain: weather.rain,
+                                sentence: sentence
+                            })
+                        }
+                    })
+                } else {
+                    res.send('No existen coordenadas')
+                }
+            }
+        })
+    }
+})
+
+app.get('*', function(req, res) {
+    res.send({
+        error: 'Oops! Ruta no valida'
+    })
+})
+
+app.listen(port, function() {
+    console.log('up and running!')
+})
---
+++ climate.js
@@ -0,0 +1,65 @@
+const request = require('request')
+
+if (process.env.NODE_ENV === 'production') {
+    var darkSkyApiKey = process.env.DARKSKY_APIKEY
+    var mapBoxApiKey = process.env.MAPBOX_APIKEY
+} else {
+    const credentials = require('./credentials.js')
+    var darkSkyApiKey = credentials.darkSkyApiKey
+    var mapBoxApiKey = credentials.mapBoxApiKey
+}
+
+const getWeather = function(coordinates, callback) {
+    const url = 'https://api.darksky.net/forecast/' + darkSkyApiKey + '/' 
+                + coordinates.lat + ',' + coordinates.long + '?lang=es&units=si'
+
+    request({ url, json: true }, function(error, response) {
+        if (error) {
+            callback('Not Found', undefined)
+        } else {
+            const data = response.body
+
+            if (data.error) {
+                callback(data.error, undefined)
+            } else {
+                const weather = {
+                    summary: data.hourly.summary,
+                    temperature: data.currently.temperature,
+                    apparentTemperature: data.currently.apparentTemperature,
+                    rain: data.currently.precipProbability
+                }
+
+                callback(undefined, weather)
+            }
+        }
+    })
+}
+
+const getGeocode = function(city, callback) {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' 
+                 + city + '.json?access_token=' + mapBoxApiKey
+
+    request({ url, json: true }, function(error, response) {
+        if (error) {
+            callback('Not Found', undefined)
+        } else {
+            const data = response.body
+
+            if (data.message) {
+                callback(data.message, undefined)
+            } else {
+                const geometry = data.features[0].geometry
+                const coordinates = {
+                    long: geometry.coordinates[0],
+                    lat: geometry.coordinates[1]
+                }
+                callback(undefined, coordinates)
+            }
+        }
+    })
+}
+
+module.exports = {
+    getGeocode: getGeocode,
+    getWeather: getWeather
+}
